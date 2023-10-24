90a6dd5073c3aec61c1a3ecd28cc02822a537d04
Curtly Ethan Greeve <s220099006@mandela.ac.za>
2023-10-24 13:20:37 +0200

    Admin Manage areas

--- NurseVolunteeringSystem/Areas/Manager/Controllers/SuburbController.cs
+++ NurseVolunteeringSystem/Areas/Manager/Controllers/SuburbController.cs
@@ -1,118 +1,105 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using NurseVolunteeringSystem.Models;
-using NurseVolunteeringSystem;
 using Microsoft.EntityFrameworkCore;
+using NurseVolunteeringSystem.Models;
+using System.Linq;
 
-public class SuburbController : Controller
+namespace NurseVolunteeringSystem.Controllers
 {
-
-    private readonly AppDBContext _context;
-
-    public SuburbController(AppDBContext context)
+    public class SuburbController : Controller
     {
-        _context = context;
-    }
+        private readonly AppDBContext _context;
 
-    [Area("Manager")]
-
-    public IActionResult Index()
-    {
-        var suburbs = _context.Suburb.Where(c => c.Status == "Active").Include(s => s.City).ToList();
-        ViewBag.Suburbs = suburbs;
-        return View(suburbs);
-    }
-
-    [Area("Manager")]
-
-    public IActionResult Create()
-    {
-       
-        return View();
-    }
-
-    [Area("Manager")]
-
-    [HttpPost]
-    public IActionResult Create(Suburb model)
-    {
-        if (ModelState.IsValid)
+        public SuburbController(AppDBContext context)
         {
-            _context.Suburb.Add(model);
-            _context.SaveChanges();
+            _context = context;
+        }
+        [Area("Manager")]
+
+        public IActionResult Index()
+        {
+            var suburbs = _context.Suburb.Where(s => s.Status == "Active").Include(s => s.City).ToList(); 
+            ViewBag.Suburbs = suburbs;
+            return View(suburbs);
+        }
+        [Area("Manager")]
+        public IActionResult Create()
+        {
+            ViewBag.Cities = _context.City.ToList();
+            return View();
+        }
+        [Area("Manager")]
+        [HttpPost]
+        public IActionResult Create(Suburb model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Suburb.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Cities = _context.City.ToList();
+            return View(model);
+        }
+        [Area("Manager")]
+        public IActionResult Edit(int id)
+        {
+            var suburb = _context.Suburb.Include(s => s.City).FirstOrDefault(s => s.SuburbID == id);
+            if (suburb == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Cities = _context.City.ToList();
+            return View(suburb);
+        }
+
+        [Area("Manager")]
+        [HttpPost]
+        public IActionResult Edit(Suburb model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Suburb.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Cities = _context.City.ToList();
+            return View(model);
+        }
+
+        [Area("Manager")]
+        public IActionResult Details(int id)
+        {
+            var suburb = _context.Suburb.Include(s => s.City).FirstOrDefault(s => s.SuburbID == id);
+            if (suburb == null)
+            {
+                return NotFound();
+            }
+            return View(suburb);
+        }
+
+        [Area("Manager")]
+
+        public IActionResult Delete(int id)
+        {
+            var suburb = _context.Suburb.Include(s => s.City).FirstOrDefault(s => s.SuburbID == id);
+            if (suburb == null)
+            {
+                return NotFound();
+            }
+            return View(suburb);
+        }
+        [Area("Manager")]
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var suburb = _context.Suburb.Find(id);
+            if (suburb != null)
+            {
+                suburb.Status = "In-Active";
+                _context.Suburb.Update(suburb);
+                _context.SaveChanges();
+            }
             return RedirectToAction("Index");
         }
-        return View(model);
-      
     }
-
-    [Area("Manager")]
-
-    public IActionResult Edit(int id)
-    {
-        var suburb = _context.Suburb.Find(id);
-        if (suburb == null)
-        {
-            return NotFound();
-        }
-        return View(suburb);
-    }
-
-    [Area("Manager")]
-
-    [HttpPost]
-    public IActionResult Edit(Suburb model)
-    {
-        if (ModelState.IsValid)
-        {
-            _context.Suburb.Update(model);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        return View(model);
-    }
-
-    [Area("Manager")]
-
-    public IActionResult Details(int id)
-    {
-        var suburb = _context.Suburb.Find(id);
-        if (suburb == null)
-        {
-            return NotFound();
-        }
-        return View(suburb);
-    }
-
-    [Area("Manager")]
-    public IActionResult Delete(int id)
-    {
-        var suburb = _context.Suburb.Find(id);
-        if (suburb == null)
-        {
-            return NotFound();
-        }
-        return View(suburb);
-    }
-
-    [Area("Manager")]
-
-    [HttpPost, ActionName("Delete")]
-    public IActionResult DeleteConfirmed(int id)
-    {
-        var suburb = _context.Suburb.Find(id);
-        if (suburb != null)
-        {
-            suburb.Status = "In-Active";
-            _context.Suburb.Update(suburb);
-            _context.SaveChanges();
-        }
-        return RedirectToAction("Index");
-    }
-
-
 }
-
-
