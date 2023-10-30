45d855ecf2716eb044aee25eb1efd591a0bf6bbc
Curtly Ethan Greeve <s220099006@mandela.ac.za>
2023-10-30 17:09:22 +0200

    Preffered Suburb Report

---
+++ NurseVolunteeringSystem/Areas/Manager/Controllers/PrefferedSuburb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NurseVolunteeringSystem.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NurseVolunteeringSystem.Areas.Manager.Controllers
+{
+    [Area ("Manager")]
+    [Route("PrefferedSuburb")]
+    public class PrefferedSuburbController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public PrefferedSuburbController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var prefferedSuburbs = _context.PrefferedSuburb.ToList(); 
+
+            return View(prefferedSuburbs);
+        }
+
+       
+    }
+
+}
